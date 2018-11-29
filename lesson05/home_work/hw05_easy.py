0912767430b38ffde40ef012b96cee6231185fea
Artem <artela1992@gmail.com>
2018-11-29 17:32:59 +0300

    less 5 normal

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -4,27 +4,49 @@
 # И второй скрипт, удаляющий эти папки.
 import os
 from shutil import copyfile
-for i in range(1,10):
-    try:
-        os.mkdir(f"dir_{i}")
-    except FileExistsError:
-        #print(f"Error: dir{i} already exist")
-        if len(os.listdir(f"dir_{i}")) == 0:
-            os.rmdir(f"dir_{i}")
-            #print("123")
-#os.removedirs(path)
+def addDir(name='dir',count=1):
+    count=int(count)
+    dir_names = []
+    if count>1:
+        for i in range(1,count+1):
+            try:
+                os.mkdir(f"{name}_{i}")
+            except FileExistsError:
+                if len(os.listdir(f"{name}_{i}")) == 0:
+                    continue
+            dir_names.append(f"{name}_{i}")
+                   # os.rmdir(f"{name}_{i}")
+
+    else:
+        try:
+            os.mkdir(f"{name}")
+        except FileExistsError:
+            return 'Данная папка существует'
+
+    if dir_names.__len__()==0:
+        return f'Папка {name} создана'
+    else:
+        dir_names = ' , '.join(dir_names)
+        return f'Папки {dir_names} созданы'
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-for path in os.listdir(os.getcwd()):
-    if os.path.isdir(path):
-        print(path)
-# Задача-3:
-# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-copy_file = "copy.py"
-if not os.path.exists(copy_file):
-    copyfile(os.path.basename(__file__),copy_file)
-else:
-     for path in os.listdir(os.getcwd()):
-         if os.path.isfile(path=path):
-             if os.path.split(path)[1]==copy_file:
-                 os.remove(path)
+def get_thisDir():
+    result = []
+    for path in os.listdir(os.getcwd()):
+        if os.path.isdir(path):
+            result.append(f'Папка:{path}')
+        elif os.path.isfile(path):
+            result.append(f'Файл:{path}')
+    result ='\n'.join(result)
+    return result
+# # Задача-3:
+# # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+# copy_file = "copy.py"
+# if not os.path.exists(copy_file):
+#     copyfile(os.path.basename(__file__),copy_file)
+# else:
+#      for path in os.listdir(os.getcwd()):
+#          if os.path.isfile(path=path):
+#              if os.path.split(path)[1]==copy_file:
+                 #os.remove(path)
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -1,4 +1,4 @@
-# Задача-1:
+## Задача-1:
 # Напишите небольшую консольную утилиту,
 # позволяющую работать с папками текущей директории.
 # Утилита должна иметь меню выбора действия, в котором будут пункты:
@@ -13,3 +13,55 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+import argparse
+import sys
+import hw05_easy
+
+
+def get_arg():
+    pars = argparse.ArgumentParser()
+    pars.add_argument('-u','--util',default='1')
+    return pars
+
+def get_command(command=0):
+    a = get_arg()
+    a = a.parse_args(sys.argv[1:])
+    if command==0:
+        if a.util:
+            command = int(input("1. Перейти в папку\n"+
+                            "2. Просмотреть содержимое текущей папки\n"+
+                            "3. Удалить папку\n"+
+                            "4. Создать папку\n"))
+            if command=='':
+                get_command(command=0)
+    elif command >= 1 and command <=4:
+        return int(command)
+    else:
+        command=int(input("Команда введена не верно, повторите попытку: \n"+
+                                "1. Перейти в папку\n" +
+                                "2. Просмотреть содержимое текущей папки\n" +
+                                "3. Удалить папку\n" +
+                                "4. Создать папку\n"))
+        if command == '':
+            get_command(command=0)
+    return command
+
+
+def util(command):
+
+    if command==1:
+        pass
+    elif command==2:
+        return hw05_easy.get_thisDir()
+    elif command==3:
+        pass
+    elif command==4:
+        name = input('Введите имя')
+        count = input('и количество(необязтельно)')
+        if count=='':
+            count=1
+        if name == '':
+            util(command)
+        return hw05_easy.addDir(name,count)
+command =get_command()
+print(util(command))
